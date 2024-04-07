79f607891462181cf7817472d28cbfe8203b102c
Ashwin Singh <nisa397@aucklanduni.ac.nz>
2024-04-07 15:47:27 +1200

    added constructor to booking class

--- src/main/java/nz/ac/auckland/se281/Booking.java
+++ src/main/java/nz/ac/auckland/se281/Booking.java
@@ -6,4 +6,12 @@ public class Booking{
   String requestedDate;
   String email;
   String attendees;
+
+  public Booking(String venueCode, String requestedDate, String email, String attendees){
+    this.venueCode=venueCode;
+    this.requestedDate=requestedDate;
+    this.email=email;
+    this.attendees=attendees;
+  }
+
 }
