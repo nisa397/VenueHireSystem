e6b6f240afcccab9a708570fd082a1a8547d35bd
Ashwin Singh <nisa397@aucklanduni.ac.nz>
2024-04-11 12:50:10 +1200

    created abstract service class, and added instance methods

---
+++ src/main/java/nz/ac/auckland/se281/Service.java
@@ -0,0 +1,35 @@
+package nz.ac.auckland.se281;
+import java.util.ArrayList;
+
+public abstract class Service{
+  String inpBookingRef;
+  int cost;
+
+  /*public Service (String inpBookingRef){
+    this.inpBookingRef=inpBookingRef;
+
+
+
+
+
+  }*/
+
+
+
+
+
+  abstract public int calculatingCost();
+
+  public boolean doesRefExist(ArrayList <Booking> allBookings, String inpBookingRef){
+
+    for (Booking booking: allBookings){
+
+      if (booking.bookingRef.equals(inpBookingRef)){
+        return true;
+
+      }
+    }
+
+    return false;
+  }
+}
